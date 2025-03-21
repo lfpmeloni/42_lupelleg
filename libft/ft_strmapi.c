dfca467a10c3d00e3691e0a666aaeb262147f48c
lfpmeloni <felipemeloni@hotmail.com>
2025-03-21 16:55:46 +0100

    final commit prior eval

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,12 +6,12 @@
 /*   By: lupelleg <lupelleg@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 16:45:09 by lupelleg          #+#    #+#             */
-/*   Updated: 2025/03/19 09:57:17 by lupelleg         ###   ########.fr       */
+/*   Updated: 2025/03/21 16:35:57 by lupelleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
- *  Applies the function f to each character of the string s, passing ites index
+ *  Applies the function f to each character of the string s, passing its index
  *  as the first argument and the character itself as the second. A new string
  *  is created using malloc(3) to store the results from the successive
  *  applications of f. 
@@ -34,7 +34,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	while (s[i])
 	{
-		new_str[i] = f(i, *s);
+		new_str[i] = f(i, s[i]);
 		i++;
 	}
 	new_str[i] = '\0';
