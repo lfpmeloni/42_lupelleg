c34b9e2c8c073716d74e7fd62f830ca7dd596a8b
lfpmeloni <felipemeloni@hotmail.com>
2025-03-27 17:06:02 +0100

    changes to parsign of flags

--- ft_printf/ft_parse_flags.c
+++ ft_printf/ft_parse_flags.c
@@ -6,7 +6,7 @@
 /*   By: lupelleg <lupelleg@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 11:35:39 by lupelleg          #+#    #+#             */
-/*   Updated: 2025/03/24 11:36:33 by lupelleg         ###   ########.fr       */
+/*   Updated: 2025/03/27 17:05:29 by lupelleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,56 @@ static int	ft_parse_width(t_flags *flags, const char *format, va_list args)
 	while (ft_isdigit(format[i]))
 	{
 		flags->width = (flags->width * 10) + (format[i] - '0');
+		i++;
+	}
+	return (i);
+}
+
+static int	ft_parse_precision(t_flags *flags, const char *format, va_list args)
+{
+	int	i;
+
+	i = 0;
+	if (format[i] != '.')
+		return (0);
+	i++;
+	flags->precision = 0;
+	if (format[i] == '*')
+	{
+		flags->precision = va_arg(args, int);
+		if (flags->precision < 0)
+			flags->precision = -1;
+		return (1);
+	}
+	while (ft_isdigit(format[i]))
+	{
+		flags->precision = (flags->precision * 10) + (format[i] - '0');
+		i++;
+	}
+	return (i);
+}
+
+int	ft_parse_flags(t_flags *flags, const char *format, va_list args)
+{
+	int	i;
+
+	i = 0;
+	ft_init_flags(flags);
+	while (format[i])
+	{
+		if (format[i] == '-')
+			flags->minus = 1;
+		if (format[i] == '0' && !flags->minus)
+			flags->zero = 1;
+		if (ft_isdigit(format[i]) || format[i] == '*')
+			i += ft_parse_width(flags, &format[i], args);
+		if (format[i] == '.')
+			i += ft_parse_precision(flags, &format[i], args);
+		if (ft_strchr("cspdiuxX%", format[i]))
+		{
+			flags->type = format[i];
+			return (i);
+		}
 		i++;
 	}
 	return (i);
