d534d2f3e609287e17eb1b3ae4f22f1828189f6b
lfpmeloni <felipemeloni@hotmail.com>
2025-03-27 14:17:40 +0100

    basic functions structure

---
+++ ft_printf/ft_print_unsigned_int.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_unsigned_int.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lupelleg <lupelleg@student.42heilbronn.de  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 14:11:30 by lupelleg          #+#    #+#             */
+/*   Updated: 2025/03/27 14:15:46 by lupelleg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int		ft_print_unsigned_int(t_flags *flags, unsigned int n)
+{
+}
--- ft_printf/libftprintf.h
+++ ft_printf/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: lupelleg <lupelleg@student.42heilbronn.de  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 10:12:33 by lupelleg          #+#    #+#             */
-/*   Updated: 2025/03/25 08:01:33 by lupelleg         ###   ########.fr       */
+/*   Updated: 2025/03/27 14:13:18 by lupelleg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,12 @@ typedef struct s_flags
 int		ft_printf(const char *format, ...);
 int		ft_parse_flags(t_flags *flags, const char *format, va_list args);
 int		ft_print_type(t_flags *flags, va_list args);
+int		ft_print_char(t_flags *flags, int c);
+int		ft_print_string(t_flags *flags, char *str);
+int		ft_print_pointer(t_flags *flags, unsigned long long n);
+int		ft_print_int(t_flags *flags, int n);
+int		ft_print_unsigned_int(t_flags *flags, unsigned int n);
+int		ft_print_hex(t_flags *flags, unsigned int n);
+int		ft_print_percent(t_flags *flags);
 
 #endif
